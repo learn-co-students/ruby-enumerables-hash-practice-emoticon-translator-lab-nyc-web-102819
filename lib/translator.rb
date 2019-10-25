84b5b04c7e9419f008dd997d38b4162104ab1f0d
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-25 17:39:45 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,28 +1,50 @@
 # require modules here
 require 'yaml'
+require 'pry'
+
 
 def load_library(file_path)
   # code goes here
   doc = YAML.load_file(file_path)
-
+#binding.pry
   doc
 
-  #YAML.dump(file_path)
-   #{ a: 'b'}.to_yaml
+  hash = {"get_meaning" => {},"get_emoticon" => {}}
+
+  doc.each do |key, value|
+    hash["get_meaning"][value[1]] = key
+    hash["get_emoticon"][value[0]] = doc[key][1]
+
+  end
+hash
 
 end
 
-def get_japanese_emoticon(file_path,english_emoticon)
+def get_japanese_emoticon(file_path, english_emoticon)
   # code goes here
-hash = {english_emoticon: {}}
 
-hash
+  x = load_library(file_path)
+
+
+  hash = x["get_emoticon"][english_emoticon]
+
+  if hash == nil
+    return "Sorry, that emoticon was not found"
+  end
+  hash
+
 end
 
 def get_english_meaning(file_path, japanese_emoticon)
   # code goes here
-  hash = {japanese_emoticon: {}}
+  x =  load_library(file_path)
 
+  hash = x["get_meaning"][japanese_emoticon]
+
+
+  if hash == nil
+    return "Sorry, that emoticon was not found"
+  end
   hash
 
 end
