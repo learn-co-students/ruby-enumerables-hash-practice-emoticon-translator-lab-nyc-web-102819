1c4af4e85eba82d7dda611945301e9bdd0b95d63
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-25 15:58:44 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,28 @@
 # require modules here
+require 'yaml'
 
-def load_library
+def load_library(file_path)
   # code goes here
+  doc = YAML.load(file_path)
+
+  doc
+
+  #YAML.dump(file_path)
+   #{ a: 'b'}.to_yaml
+
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path,english_emoticon)
   # code goes here
+hash = {english_emoticon: {}}
+
+hash
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, japanese_emoticon)
   # code goes here
+  hash = {japanese_emoticon: {}}
+
+  hash
+
 end
