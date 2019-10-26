1c6687bdecdfc3a01e802cc106c9b14e2d023044
Gabe Gutierrez <gabedotgutierrez@gmail.com>
2019-10-26 07:24:29 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,11 +19,11 @@ end
 def get_japanese_emoticon(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
   japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
-  japanese_emoticon ? japanese_emoticon : 'Help!'
+  japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
 end
 
 def get_english_meaning(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
   english_meaning = emoticon_lib['get_meaning'][emoticon]
-  english_meaning ? english_meaning : 'Dang lets try again.'
+  english_meaning ? english_meaning : 'Sorry, that emoticon was not found'
 end
